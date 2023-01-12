13e361c759a129f8c81cc3286d0f0d17bf3a0ef9
Risyandi <risyandi.94@gmail.com>
2023-01-12 17:41:50 +0700

    fix: modified file

--- rabbitmq/golang/tutorial-three/consumer/receiveLog.go
+++ rabbitmq/golang/tutorial-three/consumer/receiveLog.go
@@ -6,6 +6,7 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// function to handle error
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
@@ -13,14 +14,17 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
+	// connected to rabbitMQ
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
+	// connected to channel
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
+	// exchange from producer with type exchange "fanout" with name is "logs"
 	err = ch.ExchangeDeclare(
 		"logs",   // name
 		"fanout", // type
@@ -32,6 +36,7 @@ func main() {
 	)
 	failOnError(err, "Failed to declare an exchange")
 
+	// declare queue with no name because name generate from rabbitMQ for the example of generate name  queue : amq.gen-JzTY20BRgKO-HjmUJj0wLg.
 	q, err := ch.QueueDeclare(
 		"",    // name
 		false, // durable
@@ -42,6 +47,7 @@ func main() {
 	)
 	failOnError(err, "Failed to declare a queue")
 
+	// queue binding
 	err = ch.QueueBind(
 		q.Name, // queue name
 		"",     // routing key
@@ -51,6 +57,7 @@ func main() {
 	)
 	failOnError(err, "Failed to bind a queue")
 
+	// consume queue
 	msgs, err := ch.Consume(
 		q.Name, // queue
 		"",     // consumer
