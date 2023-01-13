eb9f4a14e4cd55a76e26e1835dba08a12545534c
Risyandi <risyandi.94@gmail.com>
2023-01-13 08:15:32 +0700

    fix: modified emitLog

--- rabbitmq/golang/tutorial-four/publisher/emitLogDirect.go
+++ rabbitmq/golang/tutorial-four/publisher/emitLogDirect.go
@@ -10,12 +10,14 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// function to handle error
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
 	}
 }
 
+// function to handle bodyfrom
 func bodyFrom(args []string) string {
 	var s string
 	if (len(args) < 3) || os.Args[2] == "" {
@@ -26,6 +28,7 @@ func bodyFrom(args []string) string {
 	return s
 }
 
+// function to handle severityfrom
 func severityFrom(args []string) string {
 	var s string
 	if (len(args) < 2) || os.Args[1] == "" {
@@ -37,14 +40,17 @@ func severityFrom(args []string) string {
 }
 
 func main() {
+	// connected to rabbitMQ
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
+	// connected to channel
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
+	// declare exchange type
 	err = ch.ExchangeDeclare(
 		"logs_direct", // name
 		"direct",      // type
@@ -56,9 +62,11 @@ func main() {
 	)
 	failOnError(err, "Failed to declare an exchange")
 
+	// define timeout process
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	// publish context
 	body := bodyFrom(os.Args)
 	err = ch.PublishWithContext(ctx,
 		"logs_direct",         // exchange
